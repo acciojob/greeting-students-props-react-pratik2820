e0cc0f69efa9060697dff17a2efa357823914761
pratik2820 <130150715+pratik2820@users.noreply.github.com>
2023-09-19 17:34:18 +0000

    try2

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,7 +6,8 @@ const Welcome = ({name}) =>{
     return(
 
         <div>
-            <h1>"Hey ! {name}" </h1>
+            <h1>"Hey !" </h1>
+            <p>{name}</p>
             <h2>"Welcome to School."</h2>
 
 
